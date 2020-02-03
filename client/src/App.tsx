1dd1f74653f9c1adf6257b3270f2761965df43e8
Viet Nguyen <vietjul@gmail.com>
2020-02-03 15:54:46 -0500

    chore: remove highchart scripts from index.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import Drawer from './components/Drawer';
-import HighchartsMap from './components/HighchartsMap';
 import HighchartsTest from './components/HighchartsTest';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-        {/* <HighchartsMap/> */}
         <HighchartsTest/>
         <Drawer/>
     </div>
---
+++ client/src/components/HighchartsTestMap.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options: Highcharts.Options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        name: 'something',
+        type: 'line',
+        data: [1, 2, 3]
+    }]
+}
+
+export default function HighchartsTest(props: HighchartsReact.Props) {
+    return (
+        <div>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+                {...props}
+            />
+        </div>
+    )
+}
