e73e1c567b007ca5c2546510a08144d8f51e2a88
Viet Nguyen <vietjul@gmail.com>
2020-02-03 09:50:38 -0500

    feat: remove earth, add mapchart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Earth from './components/Earth';
 import Drawer from './components/Drawer';
+import MapChart from './components/MapChart';
+import ReactTooltip from 'react-tooltip';
 
 const App: React.FC = () => {
+    const [content, setContent] = useState("");
   return (
     <div className="App">
-        <Earth/>
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
         <Drawer/>
     </div>
   );
--- client/src/components/Earth.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Earth() {
-    return (
-        <div id="earth_div"></div>
-    )
-}
